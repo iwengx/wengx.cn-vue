6bf4f6f48eda2618789694e8950e5613837628c3
wengx-unx <1027382710@qq.com>
2022-05-08 01:46:43 +0800

    import nes.css in the main.ts file

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,8 @@ import App from './App.vue';
 import { router } from './router/index';
 import { createPinia } from 'pinia';
 
+import 'nes.css/css/nes.min.css';
+
 const app = createApp(App);
 app.use(createPinia());
 app.use(router);
