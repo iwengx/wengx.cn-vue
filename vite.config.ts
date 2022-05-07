831b2b41b11d6634a1661c666f2e94e2fb115c5d
wengx-unx <1027382710@qq.com>
2022-05-08 01:42:06 +0800

    feat(package): add some package, including
    - nes.css is NES-style (8bit-like) CSS Framework
    - unplugin-auto-import and unplugin-vue-components

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,24 @@
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
+import AutoImport from 'unplugin-auto-import/vite';
+import Components from 'unplugin-vue-components/vite';
+import { AntDesignVueResolver } from 'unplugin-vue-components/resolvers';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-   plugins: [vue()],
+   plugins: [
+      vue(),
+      AutoImport({
+         imports: ['vue', 'vue-router', 'pinia'],
+      }),
+      Components({
+         resolvers: [AntDesignVueResolver()],
+      }),
+   ],
    server: {
       open: true,
       host: '0.0.0.0',
-      port: 3334,
+      port: 3335,
    },
    build: {
       // 打包后的默认大小，超出则跳出警告内容
