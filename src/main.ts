8368dc79724f89ce32bfaf4008cd884ef5d6e9f8
wengx-unx <1027382710@qq.com>
2022-05-08 18:13:53 +0800

    style: add some style file and imported

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,9 @@ import { router } from './router/index';
 import { createPinia } from 'pinia';
 
 import 'nes.css/css/nes.min.css';
+import './style/global/base-animation.scss';
+import './style/global/width-media.scss';
+import './style/global/base-public.scss';
 
 const app = createApp(App);
 app.use(createPinia());
