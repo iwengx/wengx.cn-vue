13d14b5a9b8460f79a4165840aee32a8ed7df3d3
wengx-unx <1027382710@qq.com>
2022-05-08 18:15:43 +0800

    style(App): move style to global public style file

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,3 @@
 <template>
    <router-view></router-view>
 </template>
-
-<style>
-* {
-   margin: 0;
-   padding: 0;
-   box-sizing: border-box;
-   font-family: 'Zpix' !important;
-}
-
-@font-face {
-   font-family: 'Zpix';
-   src: url('./assets/font/Zpix.woff2') format('woff2');
-}
-</style>
