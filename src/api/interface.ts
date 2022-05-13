42109f21df151435ee3be8bff29690b3bdbeac97
wengx-unx <1027382710@qq.com>
2022-05-13 23:55:28 +0800

    feat(interface): add some interface function

--- src/api/interface.ts
+++ src/api/interface.ts
@@ -1,17 +1,26 @@
 import http from './http.js';
 import { AxiosPromise } from 'axios';
-import { getCookie } from '../utils/cookie/index';
 
-// 获取cookie中存储的uuid
-let accountUuid: string = getCookie('uuid') || '';
-
-// 设置uuid
-export function setAccountUuid(): void {
-   // 重新获取一次
-   accountUuid = getCookie('uuid') || '';
+/**
+ * 获取当天的Web访问量
+ * @returns
+ */
+export function getToDayWebVisitorsCount(): AxiosPromise {
+   return http.get('/web/visitor/count');
 }
 
-// 获取账号信息
-// export function getLoginAccountInfo(): AxiosPromise {
-//    return http.get('/wit-admin/account/info');
-// }
+/**
+ * 获取当天的小肝访问量
+ * @returns
+ */
+export function getToDayLilLiverHelperVisitorsCount(): AxiosPromise {
+   return http.get('/lilLiverHelper/visitor/count');
+}
+
+/**
+ * 记录当天的Web访问量
+ * @returns
+ */
+export function setToDayWebVisitorsCount(): AxiosPromise {
+   return http.post('/web/visitor/count');
+}
