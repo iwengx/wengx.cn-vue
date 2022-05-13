7c647c5d2639202c9531b438930f154741a622d6
wengx-unx <1027382710@qq.com>
2022-05-13 23:56:19 +0800

    feat(App): use setToDayWebVisitorsCount API

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,8 @@
 <template>
    <router-view></router-view>
 </template>
+
+<script setup lang="ts">
+import { setToDayWebVisitorsCount } from './api/interface';
+setToDayWebVisitorsCount();
+</script>
