e86970d8bcad989a506d1b1bcf9cc0fe1160471a
WengX <1027382710@qq.com>
2024-04-16 22:58:08 +0800

    refactor: add @ for relative path references

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
+import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -13,4 +14,10 @@ export default defineConfig({
       // 打包后的默认大小，超出则跳出警告内容
       chunkSizeWarningLimit: 1500,
    },
+   resolve: {
+      alias: {
+         '@': path.resolve(__dirname, './src'),
+      },
+   },
 });
+
