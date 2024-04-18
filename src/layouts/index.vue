5a123827081e7e5f8227e4faa2d60b761360a105
WengX <1027382710@qq.com>
2024-04-18 10:18:10 +0800

    feat: add keep-alive cache to router-view

--- src/layouts/index.vue
+++ src/layouts/index.vue
@@ -14,7 +14,11 @@
          </div>
 
          <div class="body-content wengx-scrollbar">
-            <router-view></router-view>
+            <router-view v-slot="{ Component }">
+               <keep-alive>
+                  <component :is="Component" />
+               </keep-alive>
+            </router-view>
          </div>
 
          <div class="body-footer">
