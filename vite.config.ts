a9d829ef92592e39eb8b5bca44d220378f6e9a79
WengX <1027382710@qq.com>
2024-05-11 22:17:05 +0800

    feat: add program homepage and sponsorship page

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,4 @@
+import { resolve } from 'path';
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import path from 'path';
@@ -13,6 +14,13 @@ export default defineConfig({
    build: {
       // 打包后的默认大小，超出则跳出警告内容
       chunkSizeWarningLimit: 1500,
+      rollupOptions: {
+         input: {
+            main: resolve(__dirname, 'index.html'),
+            sponsorship: resolve(__dirname, 'sponsorship.html'),
+            programhome: resolve(__dirname, 'programhome.html'),
+         },
+      },
    },
    resolve: {
       alias: {
